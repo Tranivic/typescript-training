ca0ab290e1c6339a86f5aebbf92ec3c973afbe98
Victor <victor.trani1@gmail.com>
2023-04-13 19:43:41 -0300

    feat: abstracted class implemented, code refactored

--- typescript classes/dist/short_classes_constructor.js
+++ typescript classes/dist/short_classes_constructor.js
@@ -1,27 +1,61 @@
 "use strict";
+// Enum
+var CompanySectors;
+(function (CompanySectors) {
+    CompanySectors["TECH"] = "Technology";
+    CompanySectors["AUTO"] = "Automotive";
+    CompanySectors["FOOD_SERVICE"] = "Food Service";
+    CompanySectors["HUMAN_RESOURCES"] = "Human Resources";
+})(CompanySectors || (CompanySectors = {}));
+// Abstracted class implemented
 class Company {
     constructor(name, id, sector) {
         this.name = name;
         this.id = id;
         this.sector = sector;
     }
-    printInfo() {
-        console.log(`This is the ${this.name} company, ${companySectors[this.sector]} area. (Company id: ${this.id})`);
-    }
 }
+// Static data
+Company.fiscalYear = 2023;
+// Class inheritance IT sector
 class ITCompany extends Company {
     constructor(name, id) {
-        super(name, id, companySectors.TECH);
+        super(name, id, CompanySectors.TECH);
+    }
+    printCompanyDescription() {
+        console.log(`This is the ${this.name} company, ${this.sector} sector. (Company id: ${this.id})`);
     }
 }
-var companySectors;
-(function (companySectors) {
-    companySectors[companySectors["TECH"] = 0] = "TECH";
-    companySectors[companySectors["AUTO"] = 1] = "AUTO";
-    companySectors[companySectors["FOOD_SERVICE"] = 2] = "FOOD_SERVICE";
-    companySectors[companySectors["HUMAN_RESOURCES"] = 3] = "HUMAN_RESOURCES";
-})(companySectors || (companySectors = {}));
-const fiatCompany = new Company('Fiat', 11, companySectors.AUTO);
-fiatCompany.printInfo();
+// Class inheritance Food Sector
+class FoodCompany extends Company {
+    constructor(name, id) {
+        super(name, id, CompanySectors.FOOD_SERVICE);
+        this.foodStorage = 0;
+    }
+    printCompanyDescription() {
+        console.log(`This is the ${this.name} company, ${this.sector} sector. (Company id: ${this.id})`);
+    }
+    get foodQuantity() {
+        if (this.foodStorage === 0) {
+            throw new Error('Food storage is zero (0)');
+        }
+        return this.foodStorage;
+    }
+    set setFoodQuantity(quantity) {
+        if (quantity > 0 && quantity < 100) {
+            this.foodStorage = quantity;
+        }
+        else {
+            this.foodStorage = 0;
+            throw new Error('Food quantity can not be more than 100');
+        }
+    }
+}
+// Instances
 const asusCompany = new ITCompany('Asus', 34);
-asusCompany.printInfo();
+const lactaCompany = new FoodCompany('Lacta', 12);
+// Usage of Set & Getters
+lactaCompany.setFoodQuantity = 10;
+console.log(lactaCompany.foodQuantity);
+// Usage of printCompanyDescription method
+lactaCompany.printCompanyDescription();
--- typescript classes/src/short_classes_constructor.ts
+++ typescript classes/src/short_classes_constructor.ts
@@ -1,35 +1,76 @@
-// Class short writed
-class Company {
+// Enum
+enum CompanySectors {
+  TECH = 'Technology',
+  AUTO = 'Automotive',
+  FOOD_SERVICE = 'Food Service',
+  HUMAN_RESOURCES = 'Human Resources',
+}
+
+// Abstracted class implemented
+abstract class Company {
+  // Static data
+  static fiscalYear = 2023;
+
   constructor(
-    public name: string,
-    public id: number,
-    public sector: companySectors
+    protected name: string,
+    protected id: number,
+    protected sector: CompanySectors
   ) {}
-  printInfo() {
+
+  public abstract printCompanyDescription(): void;
+}
+
+// Class inheritance IT sector
+class ITCompany extends Company {
+  constructor(name: string, id: number) {
+    super(name, id, CompanySectors.TECH);
+  }
+
+  public printCompanyDescription(): void {
     console.log(
-      `This is the ${this.name} company, ${
-        companySectors[this.sector]
-      } area. (Company id: ${this.id})`
+      `This is the ${this.name} company, ${this.sector} sector. (Company id: ${this.id})`
     );
   }
 }
 
-// Class inheritance
-class ITCompany extends Company {
+// Class inheritance Food Sector
+class FoodCompany extends Company {
+  private foodStorage: number = 0;
+
   constructor(name: string, id: number) {
-    super(name, id, companySectors.TECH);
+    super(name, id, CompanySectors.FOOD_SERVICE);
+  }
+
+  public printCompanyDescription(): void {
+    console.log(
+      `This is the ${this.name} company, ${this.sector} sector. (Company id: ${this.id})`
+    );
+  }
+
+  public get foodQuantity(): number {
+    if (this.foodStorage === 0) {
+      throw new Error('Food storage is zero (0)');
+    }
+    return this.foodStorage;
+  }
+
+  public set setFoodQuantity(quantity: number) {
+    if (quantity > 0 && quantity < 100) {
+      this.foodStorage = quantity;
+    } else {
+      this.foodStorage = 0;
+      throw new Error('Food quantity can not be more than 100');
+    }
   }
 }
 
-// Enums
-enum companySectors {
-  TECH,
-  AUTO,
-  FOOD_SERVICE,
-  HUMAN_RESOURCES,
-}
-
-const fiatCompany = new Company('Fiat', 11, companySectors.AUTO);
-fiatCompany.printInfo();
+// Instances
 const asusCompany = new ITCompany('Asus', 34);
-asusCompany.printInfo();
+const lactaCompany = new FoodCompany('Lacta', 12);
+
+// Usage of Set & Getters
+lactaCompany.setFoodQuantity = 10;
+console.log(lactaCompany.foodQuantity);
+
+// Usage of printCompanyDescription method
+lactaCompany.printCompanyDescription();
