1b4aaeaf08ccb6aa744ae91ef2a9ed9fef66195d
Victor <victor.trani1@gmail.com>
2023-04-19 16:10:11 -0300

    generic classis added

--- typescript generics/src/app.ts
+++ typescript generics/src/app.ts
@@ -10,16 +10,12 @@ myPromise.then((data) => {
   data.split(' ');
 });
 
-
-
 // Generic Function
 function merge<T, U>(obj1: T, obj2: U) {
   return Object.assign({}, obj1, obj2);
 }
-
 const mergedObj = merge({ oi: 'test' }, { tchau: 'test' });
 console.log(mergedObj.oi);
-
 function extractAndConvert<T extends object, U extends keyof T>(
   obj: T,
   key: U
@@ -27,5 +23,31 @@ function extractAndConvert<T extends object, U extends keyof T>(
   const returnStr = 'Value: ' + obj[key];
   return returnStr;
 }
-
 console.log(extractAndConvert({ name: 'Victor' }, 'name'));
+
+// Generic Classes
+class DataStorge<T extends number | string> {
+  constructor(public data: T[]) {}
+
+  addItem(newItem: T) {
+    this.data.push(newItem);
+  }
+
+  removeItem(newItem: T) {
+    if ((this.data.indexOf(newItem), 1)) {
+      this.data.splice(this.data.indexOf(newItem), 1);
+    }
+    return;
+  }
+
+  getItems() {
+    console.log(this.data);
+  }
+}
+
+const stringStorage = new DataStorge<string>([]);
+stringStorage.addItem('Pedro');
+stringStorage.addItem('Maria');
+stringStorage.addItem('Felipe');
+stringStorage.removeItem('Pedro');
+stringStorage.getItems();
